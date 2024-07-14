60aa4b8a7157bc67ba5df9e90438c9256c5e68f7
Tayla Phillips <76406423+tednaphil@users.noreply.github.com>
2024-07-14 14:56:13 -0500

    Add resource route, index json render statements, and cors origins

--- app/controllers/teas_controller.rb
+++ app/controllers/teas_controller.rb
@@ -1,4 +1,15 @@
 class TeasController < ApplicationController
+  # before_action :set_tea, only: [:show, :update, :destroy]
   def index
+    @teas = Tea.all
+    render json: @teas
   end
+
+  def show
+    @tea = Tea.find(params[:id])
+    render json: @tea
+  end
+
+  # add POST, PUT/PATCH, AND DELETE endpoints
+
 end
