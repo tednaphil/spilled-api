c6d7e45b27e89549a05740f0f6bd1de055b96d25
Tayla Phillips <76406423+tednaphil@users.noreply.github.com>
2024-07-16 16:35:22 -0500

    Add and migrate taylaphillips table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_16_211600) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_16_212548) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -25,6 +25,12 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_16_211600) do
     t.text "description"
     t.text "color_description"
     t.text "taste_description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
