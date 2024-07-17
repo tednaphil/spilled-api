880bf174223c83c22e75b14c04811f04c11c6dc3
Tayla Phillips <76406423+tednaphil@users.noreply.github.com>
2024-07-16 21:22:54 -0500

    Update UserFavs controller show method

--- app/controllers/user_favs_controller.rb
+++ app/controllers/user_favs_controller.rb
@@ -1,24 +1,29 @@
 class UserFavsController < ApplicationController
     def index
-        @user_favs = User_Fav.all
+        @user_favs = UserFav.all
         render json: @user_favs
         # render json: 'user_favs'
     end
 
+    # def show
+    #     @users_favs = UserFav.find(params[:id])
+    #     render json: @users_favs
+    # end
+
     def show
-        @users_favs = User_Fav.find(params[:id])
+        @users_favs = User.find(params[:id]).user_favs
         render json: @users_favs
     end
 
-    # def create
-    #     @fav = User_Fav.new(fav_params)
+    def create
+        @fav = User_Fav.new(fav_params)
 
-    #     if @fav.save
-    #         render json: @fav, status: :created, location: @fav
-    #     else
-    #         render json: @fav.errors, status: :unprocessable_entity
-    #     end
-    # end
+        if @fav.save
+            render json: @fav, status: :created, location: @fav
+        else
+            render json: @fav.errors, status: :unprocessable_entity
+        end
+    end
 
     def fav_params
         params.require(:fav).permit(:user_id, :tea_id)
