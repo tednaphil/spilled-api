18c9531741a9054e7cbca19031357ecd06671e90
Tayla Phillips <76406423+tednaphil@users.noreply.github.com>
2024-07-16 21:14:45 -0500

    Correct user and tea model syntax errors

--- app/controllers/user_favs_controller.rb
+++ app/controllers/user_favs_controller.rb
@@ -2,11 +2,26 @@ class UserFavsController < ApplicationController
     def index
         @user_favs = User_Fav.all
         render json: @user_favs
+        # render json: 'user_favs'
     end
 
     def show
         @users_favs = User_Fav.find(params[:id])
         render json: @users_favs
+    end
+
+    # def create
+    #     @fav = User_Fav.new(fav_params)
+
+    #     if @fav.save
+    #         render json: @fav, status: :created, location: @fav
+    #     else
+    #         render json: @fav.errors, status: :unprocessable_entity
+    #     end
+    # end
+
+    def fav_params
+        params.require(:fav).permit(:user_id, :tea_id)
     end
 
 end
